281f977d5b2c0aad80f6cb2e240be1a373c046ed
ov3rk1ll <overkillerror@gmail.com>
2022-04-08 09:50:39 +0200

    deno fmt

--- maps.ts
+++ maps.ts
@@ -1,48 +1,48 @@
-import { xmlParse as parse, expandGlob } from "./deps.ts";
+import { expandGlob, xmlParse as parse } from "./deps.ts";
 
 export async function parseStrings(
-    path: string,
+  path: string,
 ): Promise<Record<string, string>> {
-    const strings: Record<string, string> = {};
-    for await (const file of expandGlob(`${path}/**/*.xml`)) {
-        const content = await Deno.readTextFile(file.path);
-        const xml = parse(content).resources as any;
-        if (!xml || !xml.hasOwnProperty('string')) {
-            continue;
-        }
-        for (const n of xml.string) {
-            strings[`@string/${n["@name"]}`] = n["#text"];
-        }
+  const strings: Record<string, string> = {};
+  for await (const file of expandGlob(`${path}/**/*.xml`)) {
+    const content = await Deno.readTextFile(file.path);
+    const xml = parse(content).resources as any;
+    if (!xml || !xml.hasOwnProperty("string")) {
+      continue;
     }
-    return strings;
+    for (const n of xml.string) {
+      strings[`@string/${n["@name"]}`] = n["#text"];
+    }
+  }
+  return strings;
 }
 
 export async function parseStringArrays(
-    path: string,
-    strings: Record<string, string>,
+  path: string,
+  strings: Record<string, string>,
 ): Promise<Record<string, string[]>> {
-    const arrays: Record<string, string[]> = {};
-    for await (const file of expandGlob(`${path}/**/*.xml`)) {
-        const content = await Deno.readTextFile(file.path);
-        const xml = parse(content).resources as any;
-        if (!xml || !xml.hasOwnProperty('string-array')) {
-            continue;
-        }
-        for (const n of xml['string-array']) {
-            const key = `@array/${n['@name']}`;
-            arrays[key] = [];
-            for (const i of (n['item'] as string[])) {
-                try {
-                    if (i.startsWith('@string/')) {
-                        arrays[key].push(strings[i]);
-                    } else {
-                        arrays[key].push(i);
-                    }
-                } catch (err) {
-                    throw new Error('Failed to parse ' + i);
-                }
-            }
-        }
+  const arrays: Record<string, string[]> = {};
+  for await (const file of expandGlob(`${path}/**/*.xml`)) {
+    const content = await Deno.readTextFile(file.path);
+    const xml = parse(content).resources as any;
+    if (!xml || !xml.hasOwnProperty("string-array")) {
+      continue;
     }
-    return arrays;
+    for (const n of xml["string-array"]) {
+      const key = `@array/${n["@name"]}`;
+      arrays[key] = [];
+      for (const i of (n["item"] as string[])) {
+        try {
+          if (i.startsWith("@string/")) {
+            arrays[key].push(strings[i]);
+          } else {
+            arrays[key].push(i);
+          }
+        } catch (err) {
+          throw new Error("Failed to parse " + i);
+        }
+      }
+    }
+  }
+  return arrays;
 }
--- run.ts
+++ run.ts
@@ -1,57 +1,60 @@
 import { argsParse, xmlParse as parse } from "./deps.ts";
-import { parseStrings, parseStringArrays } from "./maps.ts";
+import { parseStringArrays, parseStrings } from "./maps.ts";
 import { convert } from "./converter.ts";
 
 const args = argsParse(Deno.args);
 if (args._.length === 0) {
-    throw new Error("Input folder argument missing");
+  throw new Error("Input folder argument missing");
 }
-if (!args['o'] && !args['output']) {
-    throw new Error("Putput file (-o or --output) argument missing");
+if (!args["o"] && !args["output"]) {
+  throw new Error("Putput file (-o or --output) argument missing");
 }
 
-const target = args['o'] || args['output'];
+const target = args["o"] || args["output"];
 const folder = args._[0] as string;
 
 // Read strings.xml and build map
 let strings: Record<string, string> = {};
 try {
-    strings = await parseStrings(folder);
+  strings = await parseStrings(folder);
 } catch (error) {
-    if (error instanceof Deno.errors.NotFound) {
-        console.warn(`Failed to open ${error.message}`)
-    } else {
-        throw error;
-    }
+  if (error instanceof Deno.errors.NotFound) {
+    console.warn(`Failed to open ${error.message}`);
+  } else {
+    throw error;
+  }
 }
 
 // Read string-arrays from arrays.xml and build map. Replace values from strings maps
 let arrays: Record<string, string[]> = {};
 try {
-    arrays = await parseStringArrays(folder, strings);
+  arrays = await parseStringArrays(folder, strings);
 } catch (error) {
-    if (error instanceof Deno.errors.NotFound) {
-        console.warn(`Failed to open ${error.message}`)
-    } else {
-        throw error;
-    }
+  if (error instanceof Deno.errors.NotFound) {
+    console.warn(`Failed to open ${error.message}`);
+  } else {
+    throw error;
+  }
 }
 
-const filename = args['f'] || args['file'] || 'app_restrictions.xml';
+const filename = args["f"] || args["file"] || "app_restrictions.xml";
 let restrictions: Array<any> = [];
 const restrictionsPath = `${folder}/xml/${filename}`;
 try {
-    const restrictionsXml = await Deno.readTextFile(restrictionsPath);
-    restrictions = convert((parse(restrictionsXml).restrictions as any).restriction, strings, arrays);
-    const encoder = new TextEncoder();
-    const data = encoder.encode(JSON.stringify(restrictions));
-    await Deno.writeFile(target, data);
-    console.log(`Output saved to ${target}!`);
+  const restrictionsXml = await Deno.readTextFile(restrictionsPath);
+  restrictions = convert(
+    (parse(restrictionsXml).restrictions as any).restriction,
+    strings,
+    arrays,
+  );
+  const encoder = new TextEncoder();
+  const data = encoder.encode(JSON.stringify(restrictions));
+  await Deno.writeFile(target, data);
+  console.log(`Output saved to ${target}!`);
 } catch (error) {
-    if (error instanceof Deno.errors.NotFound) {
-        console.warn(`Failed to open ${restrictionsPath}`)
-    } else {
-        throw error;
-    }
+  if (error instanceof Deno.errors.NotFound) {
+    console.warn(`Failed to open ${restrictionsPath}`);
+  } else {
+    throw error;
+  }
 }
-
